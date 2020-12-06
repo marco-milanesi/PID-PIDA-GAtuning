d0f113c117e5b567b06fd2bf6945baaa7aced5d6
EdoGitMira <edoardomir99@gmail.com>
2020-12-06 21:56:09 +0100

    ipd-disturbo
    inserimento funzioni ma non utilizzo

--- pidatest.m
+++ pidatest.m
@@ -1,13 +1,10 @@
 function J = pidatest(G,dt,parms)
 
-
 s = tf('s');
-
 
 K = parms(1) + parms(2)/s + (parms(3)*s)/(1 + s*(parms(3)/parms(4))) + (parms(5)*s^2)/((1 + s*parms(5)/parms(6))^2); 
 Loop = series(K,G);
 ClosedLoop = feedback(Loop,1);
-
 
 t = 0:dt:20;
 [y,t] = step(ClosedLoop,t);
--- pidtest_dist.m
+++ pidtest_dist.m
@@ -1,10 +1,9 @@
-function J = pidtest(G,dt,parms)
+function J = pidtest_dist(G,dt,parms)
 
 s = tf('s');
 
 K = parms(1) + parms(2)/s + (parms(3)*s)/(1 + s*(parms(3)/parms(4))); 
-Loop = series(K,G);
-ClosedLoop = feedback(Loop,1);
+ClosedLoop = feedback(G,K,-1);
 
 t = 0:dt:20;
 [y,t] = step(ClosedLoop,t);
