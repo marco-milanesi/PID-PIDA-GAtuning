099a42f5532f4003ffd9a69c075cee9306adf946
EdoGitMira <e.mirandola@studenti.unibs.it>
2021-03-22 08:54:04 +0100

    parametrici genetici

--- Disturbance_Rejection_Optimization/src_dist/UserInterfacedist.m
+++ Disturbance_Rejection_Optimization/src_dist/UserInterfacedist.m
@@ -469,8 +469,8 @@ function pushbutton1_Callback(hObject, eventdata, handles)
  
  %% Genetic Algorithm Paremeters
         %Population Size of each Iteration
-        PopSize = 400;
-        MaxGeneration = 2000;
+        PopSize = 200;
+        MaxGeneration = 1800;
 
 %% PID genetic algorithm
         %{
