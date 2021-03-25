691ef46ec239c07eec460e2fc5b29cb105d9d79a
Marco Milanesi <m.milanesi004@studenti.unibs.it>
2021-03-25 17:24:33 +0100

    oscillatory system 1

--- Benchmark_System/printFromWorkspace.m
+++ Benchmark_System/printFromWorkspace.m
@@ -3,7 +3,7 @@ figure();
 %% set point plote
         t_sim = 0:0.001:2*dataStep.time;
         subplot(2,2,1);
-        plot(t_sim,step(dataStep.ClosedLoop.pid,t_sim),'r-',t_sim,step(dataStep.ClosedLoop.ipd,t_sim),'b-',t_sim,step(dataStep.ClosedLoop.dpi,t_sim),'k-',t_sim,step(dataStep.ClosedLoop.pida,t_sim),'m-');
+        plot(t_sim,step(dataStep.ClosedLoop.pid,t_sim),'r-',t_sim,step(dataStep.ClosedLoop.ipd,t_sim),'b-',t_sim,step(dataStep.ClosedLoop.dpi,t_sim),'k-',t_sim,step(dataStep.ClosedLoop.pida,t_sim),'g-');
         legend('PID','I-PD','PI-D','PIDA');
         title('reference traking');
         xlabel('time');
@@ -14,8 +14,8 @@ figure();
 %% disturbance rejection plote
         t_sim = 0:0.001:2*dataStep.time;
         subplot(2,2,3);
-        plot(t_sim,step(dataDist.Disturb.pid,t_sim),'r-',t_sim,step(dataDist.Disturb.ipd,t_sim),'b-',t_sim,step(dataDist.Disturb.dpi,t_sim),'k-',t_sim,step(dataDist.Disturb.pida,t_sim),'m-');
-        legend('PID','I-PD','PI-D','PIDA');
+        plot(t_sim,step(dataDist.Disturb.pid,t_sim),'r-',t_sim,step(dataDist.Disturb.pida,t_sim),'g-');
+        legend('PID','PIDA');
         title('disturbance rejection');
         xlabel('time');
         ylabel('amplitude');
@@ -36,12 +36,12 @@ figure();
     
 %% histograph disturbance rejection
     subplot(2,2,4);
-    Y_dist = [dataDist.pid_dist.IAE dataDist.ipd_dist.IAE dataDist.dpi_dist.IAE dataDist.pida_dist.IAE];
+    Y_dist = [dataDist.pid_dist.IAE dataDist.pida_dist.IAE];
     % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
     % To preserve the order, call the reordercats function.
     
-    X_dist = categorical({'PID','I-PD','PI-D','PIDA'});
-    X_dist = reordercats(X_dist,{'PID','I-PD','PI-D','PIDA'});
+    X_dist = categorical({'PID','PIDA'});
+    X_dist = reordercats(X_dist,{'PID','PIDA'});
     
     
     bar(X_dist,Y_dist);    
--- Test_GA/gapidPLOTS.m
+++ Test_GA/gapidPLOTS.m
@@ -13,20 +13,20 @@ population_PID = rand(popsize,3);
 population_I_PD = rand(popsize,3);
 population_PI_D = rand(popsize,3);
 
-%% PID
-options_PID = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_PID,'OutputFcn',@myfun_pid);
-[x_PID,fval_PID,exitflag,output,population_PID,scores] = ga(@(K)pidtest(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_PID);
-
-fval_PID
+% %% PID
+% options_PID = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_PID,'OutputFcn',@myfun_pid);
+% [x_PID,fval_PID,exitflag,output,population_PID,scores] = ga(@(K)pidtest(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_PID);
+% 
+% fval_PID
 %% PI-D
-options_PI_D = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_PI_D,'OutputFcn',@myfun_pid);
-[x_PI_D,fval_PI_D,exitflag,output,population_PI_D,scores] = ga(@(K)dpitest(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_PID);
+options_PI_D = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_PI_D,'OutputFcn',@myfun_pi_d);
+[x_PI_D,fval_PI_D,exitflag,output,population_PI_D,scores] = ga(@(K)dpitest(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_PI_D);
 
 fval_PI_D
 
-%% I-PD
-options_I_PD = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_I_PD,'OutputFcn',@myfun_ipd);
-[x_I_PD,fval_I_PD,exitflag,output,population_I_PD,scores] = ga(@(K)ipdtest_gapid(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_PID);
-
-fval_I_PD
+% %% I-PD
+% options_I_PD = optimoptions(@ga,'PopulationSize',popsize,'MaxGenerations',MaxGenerations,'InitialPopulation',population_I_PD,'OutputFcn',@myfun_ipd);
+% [x_I_PD,fval_I_PD,exitflag,output,population_I_PD,scores] = ga(@(K)ipdtest_gapid(G,dt,K),3,-eye(3),zeros(3,1),[],[],[],[],[],options_I_PD);
+% 
+% fval_I_PD
 
