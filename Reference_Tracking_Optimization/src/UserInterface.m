9bbebe32d8f59e10e8dd7832384ddb990c7058e4
EdoGitMira <e.mirandola@studenti.unibs.it>
2021-03-31 11:01:45 +0200

    parametri

--- Reference_Tracking_Optimization/src/UserInterface.m
+++ Reference_Tracking_Optimization/src/UserInterface.m
@@ -473,8 +473,8 @@ function pushbutton1_Callback(hObject, eventdata, handles)
         
     
         %Population Size of each Iteration
-        PopSize = 100;
-        MaxGeneration = 1200;
+        PopSize = 150;
+        MaxGeneration = 1800;
         
         
 %% PID genetic algorithm
