c24f1971fe5987090ed884898a9de74cb4c06cf2
Marco Milanesi <m.milanesi004@studenti.unibs.it>
2021-04-05 18:23:33 +0200

    IAE corretto

--- Disturbance_Rejection_Optimization/src_dist/pida_test_dist.m
+++ Disturbance_Rejection_Optimization/src_dist/pida_test_dist.m
@@ -1,22 +1,29 @@
 function J = pida_test_dist(G,dt,parms)
 s = tf('s');
 global minimun_IAE;
-K = parms(1)*(1 + 1/(parms(2)*s) + (parms(3)*s)/(1 + s*(0.0001)) + (parms(4)*s^2)/((1 + s*0.0001))^2); 
+K = parms(1)*(1 + 1/(parms(2)*s) + (parms(3)*s)/(1 + s*(0.0001)) + (parms(4)*s^2)/((1 + s*(parms(4)/parms(5))))^2); 
 ClosedLoop = minreal(feedback(G,K));
 
 stable = allmargin(G*K).Stable;
 t = 0:dt:100;
 [y,t] = step(ClosedLoop,t);
-J=sum(abs(y)*dt);
+J1=sum(abs(y)*dt);
         
 switch stable
     case 1
+        if J1< minimun_IAE
+           minimun_IAE=J1;
+        end
     otherwise 
-        if J< minimun_IAE
+        if J1< minimun_IAE
             costFunction = costFunctionAddition(G,K);
-            J = J+1.05*costFunction;
+            J1 = J1+0.1*costFunction;
         end
 end
+
+J=J1;
+
+
 end
 
 
--- Disturbance_Rejection_Optimization/src_dist/untitled.m
+++ Disturbance_Rejection_Optimization/src_dist/untitled.m
@@ -1,13 +1,17 @@
 %%  
+clear all
 s=tf('s');
 dt = 0.0001;%time step
 variable =5;
 G = (1/(1+variable*s))*exp(-s)
- 
+
+
+global minimun_IAE;
+minimun_IAE = 100;
  %% Genetic Algorithm Paremeters
         %Population Size of each Iteration
         PopSize = 50;
-        MaxGeneration = 300;
+        MaxGeneration = 100;
         
 %% PIDA genetic algorithm
         %{
@@ -22,11 +26,14 @@ G = (1/(1+variable*s))*exp(-s)
         %lower bounds lb 
 
         rng(1,'twister') % for reproducibility
-        population3 = rand(PopSize,4);
-        optionsdist3 = optimoptions(@ga,'PopulationSize',PopSize,'MaxGeneration',MaxGeneration,'InitialPopulation',population3,'OutputFcn',@myfunpidadist);
-        [controldist3,IAEdist3] = ga(@(K)pida_test_dist(G,dt,K),4,-eye(4),zeros(4,1),[],[],[],[],[],optionsdist3);
-
-        K_pidadist = controldist3(1)*(1 + 1/(controldist3(2)*s) + (controldist3(3)*s)/(1 + s*(0.0001)) + (controldist3(4)*s^2)/((1 + s*0.0001)^2)); 
+        population3 = rand(PopSize,5);
+        clear gaoutfun
+        optionsdist3 = optimoptions(@ga,'PopulationSize',PopSize,'MaxGeneration',MaxGeneration,'InitialPopulation',population3,'OutputFcn',@gaoutfun);
+        [controldist3,IAEdist3] = ga(@(K)pida_test_dist(G,dt,K),5,-eye(5),zeros(5,1),[],[],[],[],[],optionsdist3);
+        record_PIDA_dist = gaoutfun();
+        
+        
+        K_pidadist = controldist3(1)*(1 + 1/(controldist3(2)*s) + (controldist3(3)*s)/(1 + s*(0.0001)) + (controldist3(4)*s^2)/((1 + s*(controldist3(4)/controldist3(5)))^2)); 
       
         global Disturb_PIDA;
         Disturb_PIDA = feedback(G,K_pidadist);
