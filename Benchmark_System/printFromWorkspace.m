0fca8bc831fd5d3977aaf4a9b04833417f2d3971
Marco Milanesi <m.milanesi004@studenti.unibs.it>
2021-04-21 22:21:50 +0200

    EPS

--- Benchmark_System/printFromWorkspace.m
+++ Benchmark_System/printFromWorkspace.m
@@ -3,7 +3,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
 
 %% set point plote
         t_sim = 0:0.001:2*dataStep.time;
-        subplot(2,3,1);
+        subplot(2,2,1);
         plot(t_sim,step(dataStep.ClosedLoop.pid,t_sim),'r-',t_sim,step(dataStep.ClosedLoop.ipd,t_sim),'b-',t_sim,step(dataStep.ClosedLoop.dpi,t_sim),'k-',t_sim,step(dataStep.ClosedLoop.pida,t_sim),'m-');
         legend('PID','I-PD','PI-D','PIDA');
         title('reference traking');
@@ -14,7 +14,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
 
 %% disturbance rejection plote
         t_sim = 0:0.001:2*dataStep.time;
-        subplot(2,3,4);
+        subplot(2,2,3);
         plot(t_sim,step(dataDist.Disturb.pid,t_sim),'r-',t_sim,step(dataDist.Disturb.pida,t_sim),'m-');
         legend('PID','PIDA');
         title('disturbance rejection');
@@ -24,7 +24,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
         
         
 %% histograph set point
-    subplot(2,3,3);
+    subplot(2,2,2);
     Y = [dataStep.pid.IAE dataStep.ipd.IAE dataStep.dpi.IAE dataStep.pida.IAE];
     % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
     % To preserve the order, call the reordercats function.
@@ -36,7 +36,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     title('IAE Trend reference traking');
     
 %% histograph disturbance rejection
-    subplot(2,3,6);
+    subplot(2,2,4);
     Y_dist = [dataDist.pid_dist.IAE dataDist.pida_dist.IAE];
     % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
     % To preserve the order, call the reordercats function.
@@ -47,8 +47,9 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     
     bar(X_dist,Y_dist);    
     title('IAE Trend disturbance rejection');
-    
-    
+  
+ %%
+    figure()
 %% Step control variable
     G = dataStep.systemTransferFunction;
     K_pid = dataStep.Controller.pid;
@@ -62,7 +63,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     u_dpi = (c1+c2)/(1+G*c1*c2 +G*c3);
     u_ipd = (K1)/(1+G*(K1+K2));
 
-    subplot(2,3,2);
+    subplot(2,2,1);
     plot(t_sim,step(K_pid/(1+K_pid*G),t_sim),'r-',t_sim,step(u_ipd,t_sim),'b-',t_sim,step(u_dpi,t_sim),'k-',t_sim,step(K_pida/(1+K_pida*G),t_sim),'m-');
     legend('PID','I-PD','PI-D','PIDA');
     title('Control variable');
@@ -72,7 +73,7 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
 
 %% Disturbance Control variable
    
-    subplot(2,3,5);
+    subplot(2,2,3);
     plot(t_sim,step(t_sim,-feedback(dataDist.Loop.pid ,1)),'r-',t_sim,step(t_sim,-feedback(dataDist.Loop.pida ,1)),'m-');
     legend('PID','PIDA');
     title('Control variable');
@@ -81,7 +82,36 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     grid on;
     
  %% Save figure
-
     savefig(dataDist.name);
+    
+%%
+%% histograph set point
+    subplot(2,2,2);
+    Y = [dataStep.pid.IAE dataStep.ipd.IAE dataStep.dpi.IAE dataStep.pida.IAE];
+    % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
+    % To preserve the order, call the reordercats function.
+    
+    X = categorical({'PID','I-PD','PI-D','PIDA'});
+    X = reordercats(X,{'PID','I-PD','PI-D','PIDA'});
+    
+    bar(X,Y);
+    title('IAE Trend reference traking');
+    
+%% histograph disturbance rejection
+    subplot(2,2,4);
+    Y_dist = [dataDist.pid_dist.IAE dataDist.pida_dist.IAE];
+    % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
+    % To preserve the order, call the reordercats function.
+    
+    X_dist = categorical({'PID','PIDA'});
+    X_dist = reordercats(X_dist,{'PID','PIDA'});
+    
+    
+    bar(X_dist,Y_dist);    
+    title('IAE Trend disturbance rejection');
+ 
+    
+    
+    
 end
 
