05e02e317ff104c94be4acd03a4f57746142fc42
EdoGitMira <e.mirandola@studenti.unibs.it>
2021-04-21 22:34:50 +0200

    print corret 2 figure

--- Benchmark_System/printFromWorkspace.m
+++ Benchmark_System/printFromWorkspace.m
@@ -48,8 +48,9 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     bar(X_dist,Y_dist);    
     title('IAE Trend disturbance rejection');
   
- %%
-    figure()
+ %% Save figure
+    savefig(dataDist.name);
+    figure('NumberTitle', 'off', 'Name', strcat(dataDist.name,"_contol"));
 %% Step control variable
     G = dataStep.systemTransferFunction;
     K_pid = dataStep.Controller.pid;
@@ -81,43 +82,10 @@ figure('NumberTitle', 'off', 'Name', dataDist.name);
     ylabel('amplitude');
     grid on;
     
- %% Save figure
-    savefig(dataDist.name);
-<<<<<<< HEAD
-    
-%%
-%% histograph set point
-    subplot(2,2,2);
-    Y = [dataStep.pid.IAE dataStep.ipd.IAE dataStep.dpi.IAE dataStep.pida.IAE];
-    % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
-    % To preserve the order, call the reordercats function.
-    
-    X = categorical({'PID','I-PD','PI-D','PIDA'});
-    X = reordercats(X,{'PID','I-PD','PI-D','PIDA'});
-    
-    bar(X,Y);
-    title('IAE Trend reference traking');
-    
-%% histograph disturbance rejection
-    subplot(2,2,4);
-    Y_dist = [dataDist.pid_dist.IAE dataDist.pida_dist.IAE];
-    % The bar function uses a sorted list of the categories, so the bars might display in a different order than you expect. 
-    % To preserve the order, call the reordercats function.
-    
-    X_dist = categorical({'PID','PIDA'});
-    X_dist = reordercats(X_dist,{'PID','PIDA'});
-    
-    
-    bar(X_dist,Y_dist);    
-    title('IAE Trend disturbance rejection');
- 
-    
-    
-    
-=======
 
+ %% Save figure
+    savefig(strcat(dataDist.name,"_contol"));
 %% plot latex Workspace
     conversione_latex(dataStep, dataDist)
->>>>>>> a20e4ee42b32e5c53f34de08928787aee98790d5
 end
 
