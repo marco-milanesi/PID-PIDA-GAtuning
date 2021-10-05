1149318bdaddbf7b776c62ad36588f9be763a264
EdoGitMira <e.mirandola@studenti.unibs.it>
2021-10-05 15:35:36 +0200

    funz cost

--- Maximum Sensitivity/Reference_Tracking_Optimization/src/pid_bandwith.m
+++ Maximum Sensitivity/Reference_Tracking_Optimization/src/pid_bandwith.m
@@ -4,20 +4,18 @@ K = parms(1)*(1 + 1/(parms(2)*s) + (parms(3)*s)/(1 + s*(parms(3)/parms(4))));
 Loop = series(K,G);
 S=allmargin(Loop);
 W1=S.PMFrequency;
-w_crossover = getGainCrossover(Loop,1);
-switch length(w_crossover)
-    case 1
-        S = feedback(1,Loop);
-        peakGain =peakgain_research(S);
-        if peakGain > 1.40
-            if peakGain > 1.45
-                W1 = 0.5* W1;
-            else
-                W1 = 0.8*W1;
-            end
-        end
-    otherwise
-        W1 = 0.01;
+
+if S.stable == 1
+    S = feedback(1,Loop);
+    peakGain = peakgain_research(S);
+    bode(S)
+    if peakGain > 1.40
+        W1 = (peakGain - 1.40+1)*W1;
+    end
+    
+else
+    W1 = 10^-6;
 end
+
 W=1/W1;
 end
