a2423f58a8e5b95b2a9ca1e0c9507e40f9dfa5ca
Marco Milanesi <marco.milanesi.99@gmail.com>
2023-07-25 11:39:39 +0200

    Delete temp/Maximum Sensitivity directory

--- temp/Maximum Sensitivity/Disturbance_Rejection_Optimization/src_dist/peakgain_research_dist.m
+++
@@ -1,9 +0,0 @@
-function peakgain = peakgain_research_dist(S)
-
-w_v = [10^-2:10^-1:10^5];
-[mag,phs,Frequency] = bode(S, w_v);
-Magnitude = squeeze(mag);
-peakgain = max(Magnitude);
-
-end
-
--- temp/Maximum Sensitivity/Reference_Tracking_Optimization/src/peakgain_research.m
+++
@@ -1,9 +0,0 @@
-function peakgain = peakgain_research(S)
-% w_v = [10^5:1:10^8];
-% [mag,phs,Frequency] = bode(S, w_v);
-% Magnitude = squeeze(mag);
-% peakgain = max(Magnitude);
-peakgain = getPeakGain(S);
-
-end
-
